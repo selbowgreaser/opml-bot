6e84ba081ec67f194fd613a35a4cefdb3e351b0a
vktrbr <viktorbarbarich@gmail.com>
2022-02-12 21:07:25 +0300

    Написал функцию, которая рисует 3д график и линии уровня

---
+++ drawing_func.py
@@ -0,0 +1,48 @@
+import pandas as pd
+import plotly
+import plotly.graph_objects as go
+import numpy as np
+
+
+def draw_3d(dots=None, critical_dots=None) -> go.Figure:
+    """
+    Функция рисует два графика и возвращает plotly Figure/
+    Первый график 3-d поверхность, второй график - линия уровня
+
+    Parameters
+    ----------
+    dots : DataFrame
+        index is x, columns is y and values dots.loc[xi, yj] = f(xi, yj)
+
+    critical_dots : list, array, DataFrame
+        array with dots like (x, y, z)
+
+    Returns
+    -------
+        plot
+
+    Examples
+    -------
+    CNT_DOTS = 200
+
+    dots = pd.DataFrame(index=np.linspace(-10, 10, CNT_DOTS),
+                        columns=np.linspace(-1, 1, CNT_DOTS))
+
+    for x in np.linspace(-10, 10, CNT_DOTS):
+        for y in np.linspace(-1, 1, CNT_DOTS):
+            dots.loc[x, y] = f(x, y)
+    draw_3d(dots)
+    """
+
+    fig = plotly.subplots.make_subplots(rows=1, cols=2,
+                                        specs=[[{'is_3d': True}, {'is_3d': False}]],
+                                        subplot_titles=['График функции', 'Линии уровня'])
+
+    fig.add_trace(go.Surface(x=dots.index, y=dots.columns, z=dots.values, opacity=0.7), 1, 1)
+
+    fig.add_trace(go.Contour(x=dots.index, y=dots.columns, z=dots.values, opacity=0.7,
+                             contours={'start': min(dots.values.flatten()),
+                                       'end': max(dots.values.flatten()),
+                                       'size': 2},
+                             colorscale='ice'), 1, 2)
+    return fig
