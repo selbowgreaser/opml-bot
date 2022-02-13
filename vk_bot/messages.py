5aa01b44c3675676114eb3e93a3d3f296e08603d
selbowgreaser <selbowgreaser@gmail.com>
2022-02-13 14:07:05 +0300

    added method send_message for Handler added attribute message for Handler added users.db in .gitignore

--- vk_bot/messages.py
+++ vk_bot/messages.py
@@ -3,9 +3,15 @@ from vk_get_api import VKApi
 
 
 class Handler:
-    def __init__(self, user_id):
+    def __init__(self, user_id, message):
         self.user_id = user_id
+        self.message = message
         self.name, self.status = User(user_id).authorization().values()
+
+    def send_message(self, answer, keyboard=None):
+        answer = answer
+        keyboard = keyboard
+        return VKApi().send_message(self.user_id, answer, keyboard)
 
     def story(self):
         if self.status == 'welcome':
