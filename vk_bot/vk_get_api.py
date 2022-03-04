5dee3d77039b033e349b2e23e934252ae44591f3
selbowgreaser <selbowgreaser@gmail.com>
2022-03-04 21:50:18 +0300

    Добавлена документация по модулю vk_get_api.py

--- vk_bot/vk_get_api.py
+++ vk_bot/vk_get_api.py
@@ -1,11 +1,14 @@
-from config import ACCESS_TOKEN, GROUP_ID, API_VERSION
-
 import vk_api
-from vk_api.utils import get_random_id
 from vk_api.longpoll import VkLongPoll
+
+from config import ACCESS_TOKEN, GROUP_ID, API_VERSION
 
 
 class VKApi:
+    """
+    Соединение с VK и получение методов API.
+    """
+
     def __init__(self):
         self.vk_session = vk_api.VkApi(token=ACCESS_TOKEN,
                                        api_version=API_VERSION)
@@ -14,4 +17,13 @@ class VKApi:
         self.vk = self.vk_session.get_api()
 
     def send_message(self, parameters):
+        """
+        Отправка сообщения пользователю с помощью API-метода.
+
+        Parameters
+        ----------
+        parameters : dict
+            Словарь с параметрами, необходимыми для отправки сообщения пользователю.
+        """
+
         self.vk.messages.send(**parameters)
