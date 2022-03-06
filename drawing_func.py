4785d484bd7eba97a7c6828a96435b5e43cc7cf2
vktrbr <viktorbarbarich@gmail.com>
2022-03-07 00:20:54 +0300

    Доработал ограничивающую функцию в смысле создания точек и отрисовки их на графиках. написал документацию по reStructuredText.

--- drawing_func.py
+++ drawing_func.py
@@ -2,38 +2,29 @@ import pandas as pd
 import plotly
 import plotly.graph_objects as go
 import numpy as np
+import sympy as sp
 
 np.seterr('ignore')
 
 GRID_WIDTH = 1
 
 
-def draw_3d(points: pd.DataFrame,
-            rest_func_points: pd.DataFrame = None,
+def draw_3d(points_of_function: pd.DataFrame,
+            points_of_restriction: pd.DataFrame = None,
             critical_points: pd.DataFrame = None) -> go.Figure:
     """
-    ----------
-        Функция рисует два графика и возвращает plotly Figure.
-        Первый график 3-d поверхность, второй график - линия уровня
+    Функция рисует два графика и возвращает plotly Figure. Первый график 3-d поверхность, второй график - линии уровня.
+    На первом и втором графиках рисуются точки critical_points и ограничивающая функция.
 
-    Parameters
-    ----------
-        points : pd.DataFrame. Датафрейм, у которого индексы - это значения по оси x,
-                 столбцы - значения по оси y, сами значения функции это матрица values.
-        rest_func_points : pd.DataFrame с тремя столбцами x, y, z
-        critical_points : pd.DataFrame, который обязательно содержит столбцы x, y, z, types
-    Returns
-    ----------
-        go.Figure
-    Examples
-    ----------
+    Code examples::
+
         import sympy as sp
 
         x, y = sp.symbols('x y')
         func = x ** 2 - y ** 2 / 2
         restr_func = x + y
-        x_conts = (-2, 2)
-        y_conts = (-2, 2)
+        x_constr = (-2, 2)
+        y_constr = (-2, 2)
 
         data = make_df_for_drawing(func, [x, y], x_conts, y_conts, 39)
 
@@ -44,27 +35,50 @@ def draw_3d(points: pd.DataFrame,
                                'color':['yellow', 'red']})
 
 
-        rest_points = rest_func_points(func, restr_func, [x, y], x_conts, y_conts)
+        rest_points = rest_func_points(func, restr_func, [x, y], x_constr, y_constr)
         fig = draw_3d(data, rest_points, points)
-        fig
+        fig.show()
+
+        # Интересная поверхность и ограничения - окружность
+        x, y = sp.symbols('x y')
+        func = (x + y) ** 2 + y * sp.cos(2 * x * sp.pi) + x * sp.sin(2 * y * sp.pi)
+        restr_func = x ** 2  + y ** 2 - 9
+        x_constr = (-5, 3.5)
+        y_constr = (-4, 3)
+
+        data = make_df_for_drawing(func, [x, y], x_constr, y_constr, 39)
+        rest_points = rest_func_points(func, restr_func, [x, y], x_constr, y_constr)
+        fig = draw_3d(data, rest_points)
+
+        fig.show()
+
+    :param points_of_function: Датафрейм, у которого индексы - это значения по оси x,
+                               столбцы - значения по оси y, сами значения функции это матрица df.values.
+    :param points_of_restriction: Датафрейм с тремя столбцами x, y, z для ограничивающей функции
+    :param critical_points: Датафрейм, который содержить столбцы x, y, z, type, color для каждой
+                            критической точки.
+    :return: go.Figure. Фигуру в plotly, с двумя подграфиками.
+
 
     """
-    if not isinstance(points, pd.DataFrame):
+
+    if not isinstance(points_of_function, pd.DataFrame):
         raise TypeError('Массив dots не является pd.DataFrame')
 
-    min_value = min(filter(np.isfinite, points.values.flatten()))
-    max_value = max(filter(np.isfinite, points.values.flatten()))
+    min_value = min(filter(np.isfinite, points_of_function.values.flatten()))
+    max_value = max(filter(np.isfinite, points_of_function.values.flatten()))
 
     fig = plotly.subplots.make_subplots(rows=1, cols=2,
                                         specs=[[{'is_3d': True}, {'is_3d': False}]],
                                         subplot_titles=['График функции', 'Линии уровня'])
 
-    fig.add_trace(go.Surface(x=points.index,
-                             y=points.columns,
-                             z=points.values.T,
+    fig.add_trace(go.Surface(x=points_of_function.index,
+                             y=points_of_function.columns,
+                             z=points_of_function.values.T,
                              opacity=0.5,
                              showscale=False,
-                             colorscale='ice'),
+                             colorscale='ice',
+                             name='f(x, y)'),
                   row=1,
                   col=1)
 
@@ -77,40 +91,40 @@ def draw_3d(points: pd.DataFrame,
     fig.update_yaxes(title='y, у.е.', col=2, row=1, gridwidth=GRID_WIDTH, gridcolor='black',
                      zerolinecolor='black', zerolinewidth=GRID_WIDTH)
 
-    fig.add_trace(go.Contour(x=points.index,
-                             y=points.columns,
-                             z=points.values.T,
+    fig.add_trace(go.Contour(x=points_of_function.index,
+                             y=points_of_function.columns,
+                             z=points_of_function.values.T,
                              opacity=0.75,
-                             contours=
-                             {
+                             contours={
                                  'start': min_value,
                                  'end': max_value,
                                  'size': (max_value - min_value) // 15,
                              },
-                             colorscale='ice'),
+                             colorscale='ice',
+                             name='f(x, y)'),
                   row=1,
                   col=2)
 
     if critical_points is not None and len(critical_points) > 0:
 
-        for type_point, points in critical_points.groupby('types'):
-            fig.add_scatter3d(x=points.x,
-                              y=points.y,
-                              z=points.z,
+        for type_point, points_of_function in critical_points.groupby('types'):
+            fig.add_scatter3d(x=points_of_function.x,
+                              y=points_of_function.y,
+                              z=points_of_function.z,
                               mode='markers',
-                              marker=dict(size=6, color=points.color),
+                              marker=dict(size=6, color=points_of_function.color),
                               showlegend=True,
                               name=type_point
                               )
 
-            fig.add_scatter(x=points.x,
-                            y=points.y,
-                            customdata=points.z,
-                            hovertemplate='x: %{x}<br><extra></extra>' + \
-                                          'y: %{y}<br>' + \
+            fig.add_scatter(x=points_of_function.x,
+                            y=points_of_function.y,
+                            customdata=points_of_function.z,
+                            hovertemplate='x: %{x}<br><extra></extra>' +
+                                          'y: %{y}<br>' +
                                           'z: %{customdata}<br>',
                             mode='markers',
-                            marker=dict(size=10, color=points.color),
+                            marker=dict(size=10, color=points_of_function.color),
                             showlegend=False,
                             name=type_point
                             )
@@ -121,14 +135,27 @@ def draw_3d(points: pd.DataFrame,
                                   x=-0.2
                                   ))
 
-    if isinstance(rest_func_points, pd.DataFrame):
-        line = go.Scatter3d(x=rest_func_points.x,
-                            y=rest_func_points.y,
-                            z=rest_func_points.z,
-                            mode='lines',
-                            line={'width': 2, 'color': 'darkblue'},
-                            name='g(x, y)')
-        fig.add_trace(line, row=1, col=1)
+    if isinstance(points_of_restriction, pd.DataFrame):
+        line3d = go.Scatter3d(x=points_of_restriction.x,
+                              y=points_of_restriction.y,
+                              z=points_of_restriction.z,
+                              mode='lines',
+                              line={'width': 3, 'color': 'darkblue'},
+                              name='g(x, y) 3d')
+        fig.add_trace(line3d, row=1, col=1)
+
+        line = go.Scatter(x=points_of_restriction.x,
+                          y=points_of_restriction.y,
+                          customdata=points_of_restriction.z.round(7),
+                          hovertemplate='x: %{x}<br>' +
+                                        'y: %{y}<br>' +
+                                        'z: %{customdata}<extra></extra>',
+                          mode='lines',
+                          line={'width': 2, 'color': 'darkblue'},
+                          name='g(x, y)')
+
+        fig.add_trace(line, row=1, col=2)
+
     return fig
 
 
@@ -136,21 +163,27 @@ def make_df_for_drawing(func,
                         variables,
                         x_constraints: tuple,
                         y_constraints: tuple,
-                        cnt_points: int = 50) -> pd.DataFrame:
+                        cnt_points: int = 49) -> pd.DataFrame:
     """
-        Calculate the function for each pair of coordinates from the Cartesian product
-        of x_constraints and y_constraints.
-    Args
-    ------
-        func: function for drawing. Sympy expression
-        variables: Переменные, которые используются в функции
-        x_constraints: tuple, like (-10, 10). x is in (-10, 10)
-        y_constraints: tuple for y
-        cnt_points: number of points on each axis
+    Создает данные для отрисовки основной функции. Можно передать передать функцию как функцию, зависимую от двух
+    аргументов или как sympy выражение. Если вы передаете sympy выражение, то обязательно передайте variables
 
-    Returns
-    ------
-        pd.DataFrame with applying func for each pair of cartesian product x and y
+    :param func: Функция, которую нужно отрисовать
+    :param variables: список и кортеж переменных в функции func
+    :param x_constraints: ограничения по оси x
+    :param y_constraints: ограничения по оси y
+    :param cnt_points: количество точек на каждой оси. Всего получиться cnt_points ** 2 точек.
+
+    :return: Датафрейм с точками для отрисовки в draw_3d.
+    Code examples::
+
+        import sympy as sp
+
+        x, y = sp.symbols('x y')
+        func = x ** 2 - y ** 2 / 2
+        restr_func = x + y
+
+        data = make_df_for_drawing(func, [x, y], (-2, 2), (-2, 2), 39)
     """
 
     func = sp.lambdify(variables, func)
@@ -164,28 +197,30 @@ def make_df_for_drawing(func,
     x_axis = list(x_axis)
     y_axis = list(y_axis)
 
-    for x in x_axis:
-        for y in y_axis:
-            f = func(x, y)
+    for x_i in x_axis:
+        for y_i in y_axis:
+            f = func(x_i, y_i)
             if np.isfinite(f):
-                points.loc[x, y] = f
+                points.loc[x_i, y_i] = f
             else:
-                points.loc[x, y] = np.nan
+                points.loc[x_i, y_i] = np.nan
 
     return points
 
 
 def save_fig_to_pic(fig: go.Figure, path: str, extensions: list) -> None:
     """
-        Save fig in image requested formats
-    Args:
-    -----
-        :param fig: the Figure to be saved
-        :param path: path without filename extension
-        :param extensions: formats - list of formats
-    Examples
-    ------
+    Сохраняет график в нужных форматах
+
+    :param fig: какой plotly график нужно сохранить
+    :param path: путь с названием файла для сохранения без расширения
+    :param extensions: список расширений
+    :return: None
+
+    Code examples::
+
         save_fig_to_pic(fig, 'plot_3d', ['png', 'jpeg', 'html'])
+
     """
 
     if 'html' in extensions:
@@ -200,52 +235,120 @@ def rest_func_points(func,
                      restr_func,
                      variables,
                      x_const: tuple,
-                     y_const: tuple) -> pd.DataFrame:
+                     y_const: tuple,
+                     cnt_points: int = 40) -> pd.DataFrame:
+    """
+    Создает данные для отрисовки ограничивающей функции
+    ФУНКЦИИ ДОЛЖНЫ БЫТЬ sympy выражениями
+
+    Code examples::
+
+        import sympy as sp
+        x, y = sp.symbols('x y')
+        func = x ** 2 - y ** 2 / 2
+        restr_func = x + y
+        rest_points = rest_func_points(func, restr_func, [x, y], (-2, 2), (-2, 2))
+
+    :param func: Функция, поверхность, которой построена
+    :param restr_func: Ограничивающая функция
+    :param variables: Список с переменными
+    :param x_const: Ограничения по оси x
+    :param y_const: Ограничения по оси y
+    :param cnt_points: Количество точек на каждой оси. АККУРАТНО, долго будет работать код на больших значениях
+    :return: pd.DataFrame с точками для отрисовки. Столбцы x, y, z
+
 
     """
-    ------
-        Создает данные для отрисовки ограничивающей функции
-        ФУНКЦИИ ДОЛЖНЫ БЫТЬ sympy выражениями
-    Args
-    ------
-        func: Функция, поверхность, которой построена
-        restr_func: Ограничивающая функция
 
-    :Return:
-    ------
-        pd.DataFrame с точками для отрисовки. Столбцы x, y, z
-    """
-    x, y = variables
+    real_variables = sp.symbols('x y', real=True)
+    func = func.subs(dict(zip(variables, real_variables)))
+    restr_func = restr_func.subs(dict(zip(variables, real_variables)))
+    epsilon_x = (x_const[1] - x_const[0]) / cnt_points
+    epsilon_y = (y_const[1] - y_const[0]) / cnt_points
+
+    x, y = real_variables
 
     out_df = pd.DataFrame(columns=['x', 'y', 'z'])
+
     i = 0
-    for x_i in np.linspace(x_const[0], x_const[1], 99):
-        y_solve = sp.solve(restr_func.subs({x: x_i}), [y])
-        for y_i in y_solve:
-            y_i = float(y_i)
-            if y_const[0] < y_i < y_const[1]:
+    for y_i in np.linspace(y_const[0], y_const[1], cnt_points):
+        try:
+            x_solve = sp.solve(restr_func.subs({y: y_i}), [x])
+        except NotImplementedError:
+            try:
+                x_solve = [sp.nsolve(restr_func.subs({y: y_i}), x, x_const[0])]
+            except ValueError:
+                x_solve = []
+
+        for x_i in x_solve:
+            x_i = float(x_i)
+            if x_const[0] <= x_i <= x_const[1]:
                 out_df.loc[i] = [x_i, y_i, float(func.subs({x: x_i, y: y_i}))]
                 i += 1
-    return out_df
+
+    for x_i in np.linspace(x_const[0], x_const[1], cnt_points):
+        try:
+            y_solve = sp.solve(restr_func.subs({x: x_i}), [y])
+        except NotImplementedError:
+            try:
+                y_solve = [sp.nsolve(restr_func.subs({x: x_i}), [y], y_const[0])]
+            except ValueError:
+                y_solve = []
+
+        for y_i in y_solve:
+            y_i = float(y_i)
+            if y_const[0] <= y_i <= y_const[1]:
+                out_df.loc[i] = [x_i, y_i, float(func.subs({x: x_i, y: y_i}))]
+                i += 1
+
+    out_df = out_df.sort_values('x').reset_index(drop=True)
+
+    len_ways = pd.DataFrame(index=out_df.index,
+                            columns=out_df.index)
+
+    for i, dot1 in enumerate(out_df[['x', 'y']].apply(tuple, axis=1)):
+        for j, dot2 in enumerate(out_df[['x', 'y']].apply(tuple, axis=1)):
+            if i == j:
+                len_ways.iloc[i, j] = np.inf
+            else:
+                len_ways.iloc[i, j] = (dot1[0] - dot2[0]) ** 2 + (dot1[1] - dot2[1]) ** 2
+
+    len_ways = len_ways.astype(float)
+    way = [0]
+    for i in range(len_ways.shape[0] - 1):
+        source = way[-1]
+        target, length = len_ways.loc[source].idxmin(), len_ways.loc[source].min()
+        len_ways.loc[source] = np.inf
+        len_ways.loc[:, source] = np.inf
+        if length > (epsilon_x + epsilon_y):
+            break
+        way.append(target)
+
+    if check_dot_in_round(out_df.loc[way[0], ['x', 'y']].values,
+                          out_df.loc[way[-1], ['x', 'y']].values,
+                          epsilon_x,
+                          epsilon_y):
+        way.append(0)
+
+    return out_df.loc[way]
 
 
-if __name__ == '__main__':
-    import sympy as sp
+def check_dot_in_round(center, check_point, radius_x, radius_y) -> bool:
+    """
+    Вспомогательная функция для rest_func_points
+    Проверяет, входит ли точка check_point в малую окрестность точки center
+    :param center: Основная точка, окрестность которой рассматриваем
+    :param check_point: Точка, которую проверяем
+    :param radius_x: значение по оси x
+    :param radius_y: значение по оси y
+    :return: False - точка не входит, True - точка входит
+    """
+    flag_x = False
+    if center[0] - radius_x <= check_point[0] <= center[0] + radius_x:
+        flag_x = True
 
-    x, y = sp.symbols('x y')
-    func = x ** 2 - y ** 2 / 2
-    restr_func = x + y
-    x_conts = (-2, 2)
-    y_conts = (-2, 2)
+    flag_y = False
+    if center[1] - radius_y <= check_point[1] <= center[1] + radius_y:
+        flag_y = True
 
-    data = make_df_for_drawing(func, [x, y], x_conts, y_conts, 39)
-
-    points = pd.DataFrame({'x': [0, 1],
-                           'y': [0, 1],
-                           'z': [0, 2],
-                           'types': ['saddle', 'Пример точки'],
-                           'color': ['yellow', 'red']})
-
-    rest_points = rest_func_points(func, restr_func, [x, y], x_conts, y_conts)
-    fig = draw_3d(data, rest_points, points)
-    save_fig_to_pic(fig, 'figure', ['html'])
+    return flag_x * flag_y
