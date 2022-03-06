3eaa6c18881b55c45cfc493bc414b8cf2c78c9bc
selbowgreaser <selbowgreaser@gmail.com>
2022-03-06 03:49:20 +0300

    Каркас модуля создан, оформлена документация.

--- vk_bot/answerer/message_handlers.py
+++ vk_bot/answerer/message_handlers.py
@@ -1,13 +1,78 @@
 from vk_api.vk_api import VkApiMethod
 
-from vk_bot.answerer.task_manager import TaskManager
+from vk_bot.answerer.keyboards import Keyboards
+from vk_bot.answerer.response_init import Response
+from vk_bot.answerer.scripted_phrases import Phrases
 from vk_bot.database import BotDatabase
 from vk_bot.user import User
 
 
-class Handlers(TaskManager):
-    def __init__(self, vk_api_method: VkApiMethod, db: BotDatabase, user: User):
-        super().__init__(vk_api_method, db, user, )
+class Handlers:
+    """
+    Генератор ответов пользователю.
 
-    def welcome(self, first_name):
-        pass
+    Parameters
+    ----------
+    vk_api_method : VkApiMethod
+        Объект соединения с VK и набор методов API.
+    db : BotDatabase
+        Объект для взаимодействия с базой данных.
+    user : User
+        Объект для взаимодействия с данными пользователя.
+    """
+
+    def __init__(self, vk_api_method: VkApiMethod, db: BotDatabase, user: User):
+        self.vk = vk_api_method
+        self.db = db
+        self.user = user
+        self.response = Response(self.user.user_id)
+
+    def greetings(self, first_name: str) -> Response:
+        """
+        Приветствие пользователя и вывод стартовой клавиатуры.
+        Статус: greetings
+
+        Parameters
+        ----------
+        first_name : str
+            Имя пользователя.
+
+        Returns
+        -------
+        Response
+            Сообщение для пользователя.
+        """
+
+        self.user.update_status('greetings')
+        self.response.set_text(Phrases.greetings.format(first_name))
+        self.response.set_keyboard(Keyboards().for_welcome())
+        return self.response
+
+    def about_me(self) -> Response:
+        """
+        Информация о боте и его разработчиках.
+        Статус: about_me
+
+        Returns
+        -------
+        Response
+            Сообщение для пользователя.
+        """
+        self.user.update_status('about_me')
+        self.response.set_text(Phrases.about_me)
+        self.response.set_keyboard(Keyboards().for_about_me())
+        return self.response
+
+    def click_button(self) -> Response:
+        """
+        Если пользователь ввёл непонятное сообщение.
+        Статус: не переопределяется
+
+        Returns
+        -------
+        Response
+            Сообщение для пользователя.
+        """
+
+        self.response.set_text(Phrases.click_button)
+        return self.response
