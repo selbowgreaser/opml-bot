a499ded379d73845f189daabd59e8f6ecf566a79
selbowgreaser <selbowgreaser@gmail.com>
2022-03-08 15:50:52 +0300

    Обновлена документация в модуле preprocessing.py

--- solver_core/search_for_extremes/handlers/preprocessing.py
+++ solver_core/search_for_extremes/handlers/preprocessing.py
@@ -1,28 +1,27 @@
 from sympy import symbols, sympify, lambdify
 from numpy import inf
 
-def prepare_data(vars, func, interval_x=None, interval_y=None, g_func = None):
+
+def prepare_data(vars, func, interval_x=None, interval_y=None, g_func=None):
     """Функция преобразовывает данные для передачи в конструктор класса.
 
     Parameters
     ----------
     vars: str
-        строка с переменными, разделенными пробелом
+        Строка с переменными, разделенными пробелом
     func: str
-        строка с фунцией
-    imterval_x: str
-        ограничения для первой переменной. границы разделенные пробелом
+        Строка с функцией
+    interval_x: str
+        Ограничения для первой переменной
     interval_y: str
-        ограничения для второй переменной
+        Ограничения для второй переменной
     g_func: str
-        ограничивающая функция в виде строки
+        Ограничивающая функция в виде строки
+
     Returns
     --------
-    vars: список из двух sympy.Symbol
-    func: sympy expression
-    interval_x: список из значений типа float или np.inf
-    imterval_y: список из значений типа dloat или np.inf
-    g_func: sympy expression
+    tuple
+        Набор параметров, подготовленный для решения задачи.
     """
 
     sympy_vars = symbols(vars)
@@ -31,6 +30,7 @@ def prepare_data(vars, func, interval_x=None, interval_y=None, g_func = None):
     func = func.subs({vars[0]: sympy_vars[0], vars[1]: sympy_vars[1]})
     interval_x = prepare_limits(interval_x)
     interval_y = prepare_limits(interval_y)
+    # TODO: возвращать словарь с параметрами, а не кортеж
     if g_func:
         g_func = sympify(g_func).subs({vars[0]: sympy_vars[0], vars[1]: sympy_vars[1]})
         return sympy_vars, func, interval_x, interval_y, g_func
@@ -39,15 +39,18 @@ def prepare_data(vars, func, interval_x=None, interval_y=None, g_func = None):
 
 
 def prepare_limits(limits):
-    """Функция преобразует ограничения в нужный вид.
+    """
+    Функция преобразует ограничения в нужный вид.
 
     Parameters
     ----------
     limits: str
-        строка с числами, разделенными пробелом
+        Строка с числами, разделенными пробелом
+
     Returns
     -------
-    list из двух float значений
+    list
+        Ограничения для функции.
     """
 
     if limits:
@@ -60,6 +63,7 @@ def prepare_limits(limits):
             else:
                 limits[i] = float(limits[i])
     return limits
+
 
 if __name__ == '__main__':
     c = prepare_data('x y', 'x**2 + y**2', '-10 10', '-10 10')
