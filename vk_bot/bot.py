26951d85a7a3192a779ad0d0fa259c04ab23477d
selbowgreaser <selbowgreaser@gmail.com>
2022-03-08 21:44:13 +0300

    Добавлен try except для того, чтобы бот не отключался.

--- vk_bot/bot.py
+++ vk_bot/bot.py
@@ -16,14 +16,17 @@ def start_work():
     print('Бот снова работает...')
     for event in vk.longpoll.listen():
         if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-            print('Получено новое сообщение!')
-            print(f'{event.user_id}: {event.text}')
-            user = User(vk.vk_api_method, db, event.user_id)
-            tm = TaskManager(vk.vk_api_method, db, user)
-            reply = tm.manage(event.text)
-            message = reply.get_message()
-            vk.send_message(message)
-            print('Сообщение успешно обработано!')
+            try:
+                print('Получено новое сообщение!')
+                print(f'{event.user_id}: {event.text}')
+                user = User(vk.vk_api_method, db, event.user_id)
+                tm = TaskManager(vk.vk_api_method, db, user)
+                reply = tm.manage(event.text)
+                message = reply.get_message()
+                vk.send_message(message)
+                print('Сообщение успешно обработано!')
+            except:
+                pass
 
 
 if __name__ == "__main__":
